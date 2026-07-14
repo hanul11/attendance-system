088ca21b042cc56f2c32e1b26d09d9dcb5057f57
hanul11 <koobongsoo6251@gmail.com>
2026-07-14 16:51:10 +0900

    feat: prepare LOGIFLOW v1.0 release

--- firebase/public/config/app-config.js
+++ firebase/public/config/app-config.js
@@ -3,10 +3,14 @@
 window.LOGIFLOW_APP_CONFIG = Object.freeze({
   appName: "LOGIFLOW",
   version: "1.0.0",
-  buildNumber: "20260714.11",
+  buildNumber: "1",
   themeColor: "#2563eb",
   backgroundColor: "#f7f8fa",
   apiUrl: "https://script.google.com/macros/s/AKfycbyd8ITba0px2wJHlzmxhkCF2Y0RpUQn9s93EJltDBKEabtL-4YJhVWqWWhL-MzvJtwY/exec",
   serviceWorkerPath: "/service-worker.js",
-  launchMode: "redirect"
+  launchMode: "embed",
+  trustedAppOrigins: Object.freeze([
+    "https://script.google.com",
+    "https://script.googleusercontent.com"
+  ])
 });
