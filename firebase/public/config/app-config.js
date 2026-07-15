c8188bed8e29bb4d1ebbc2a13b9676226945d633
hanul11 <koobongsoo6251@gmail.com>
2026-07-15 15:22:51 +0900

    chore: switch PWA launch mode to redirect

--- firebase/public/config/app-config.js
+++ firebase/public/config/app-config.js
@@ -8,7 +8,7 @@ window.LOGIFLOW_APP_CONFIG = Object.freeze({
   backgroundColor: "#f7f8fa",
   apiUrl: "https://script.google.com/macros/s/AKfycbyd8ITba0px2wJHlzmxhkCF2Y0RpUQn9s93EJltDBKEabtL-4YJhVWqWWhL-MzvJtwY/exec",
   serviceWorkerPath: "/service-worker.js",
-  launchMode: "embed",
+  launchMode: "redirect",
   trustedAppOrigins: Object.freeze([
     "https://script.google.com",
     "https://script.googleusercontent.com"
