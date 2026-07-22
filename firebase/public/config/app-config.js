5d6f111cd473ba8d42ba5397bcbfea01f6b575c2
hanul11 <koobongsoo6251@gmail.com>
2026-07-22 17:59:26 +0900

    chore(mobile): Hanul근태관리 배포 설정 통일

--- firebase/public/config/app-config.js
+++ firebase/public/config/app-config.js
@@ -1,12 +1,13 @@
 "use strict";
 
 window.LOGIFLOW_APP_CONFIG = Object.freeze({
-  appName: "한울 출퇴근 기록",
+  appName: "Hanul근태관리",
+  hostingUrl: "https://hanul-logiflow-attendance.web.app",
   version: "1.0.0",
   buildNumber: "1",
   themeColor: "#2563eb",
   backgroundColor: "#f7f8fa",
-  apiUrl: "https://script.google.com/macros/s/AKfycbwoZlxexxXTQRBAud0wnggy5vCuQfzvUcKkEAFksqmPQqHN8CFBU6T_jYS0IPXuX1cK/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwZdQADgY3SoYdTSCBhbDhhFcJpe5H8w84kDBkldoSUKcpcQgORYawg7e8WT9vr9Io/exec",
   serviceWorkerPath: "/service-worker.js",
   launchMode: "redirect",
   trustedAppOrigins: Object.freeze([
@@ -14,4 +15,3 @@ window.LOGIFLOW_APP_CONFIG = Object.freeze({
     "https://script.googleusercontent.com"
   ])
 });
-
